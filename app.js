eb07f66a077e0d8657a4a424aab37019a360ee89
Lee <leeblazek@gmail.com>
2018-01-03 15:17:09 -0700

    add module

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
+let multiply = require('./multiply');
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/multiply/:number', function(req, res){
   console.log(req.params.number);
-  let response = req.params.number*2
+  let response = multiply(req.params.number);
   res.json(response);
 });
 
